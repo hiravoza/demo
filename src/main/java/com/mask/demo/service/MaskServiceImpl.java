f71de4fc7c60c741070fe1061e6da6743ab0c3e2
Hirav Oza <hoza@rccl.com>
2020-03-28 16:16:45 -0400

    checkstyle - maven file added

--- src/main/java/com/mask/demo/service/MaskServiceImpl.java
+++ src/main/java/com/mask/demo/service/MaskServiceImpl.java
@@ -15,38 +15,24 @@ public class MaskServiceImpl implements MaskService {
 		String[] parts = email.split("@");
         
 		String strId = "";
-        if(parts[0].length() < 3)
-            strId = maskString(parts[0], 0, parts[0].length());
-        else
-            strId = maskString(parts[0], 1, parts[0].length()-1);
-        
+        if(parts[0].length() < 3) {
+			strId = maskString(parts[0], 0, parts[0].length());
+		} else {
+			strId = maskString(parts[0], 1, parts[0].length() - 1);
+		}
+
         return strId + "@" + parts[1];
 	}
 	
-	private static String maskString(String strText, int start, int end) { 
-	        if(strText == null || strText.equals(""))
-	            return "";
-	        
-	        if(start < 0)
-	            start = 0;
-	        
-	        if( end > strText.length() )
-	            end = strText.length();
-	        
-	        int maskLength = end - start;
-	        
-	        if(maskLength == 0)
-	            return strText;
-	        
+	private static String maskString(String strText, int start, int end) {
+			int maskLength = end - start;
 	        StringBuilder sbMaskString = new StringBuilder(maskLength);
 	        
 	        for(int i = 0; i < maskLength; i++){
 	            sbMaskString.append('*');
 	        }
 	        
-	        return strText.substring(0, start) 
-	                + sbMaskString.toString() 
-	                + strText.substring(start + maskLength);
+	        return strText.substring(0, start)   + sbMaskString.toString()  + strText.substring(start + maskLength);
 	    }
 
 }
