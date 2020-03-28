4e0a1cb25805a145f308a6d458e420616919ae61
Hirav Oza <hoza@rccl.com>
2020-03-28 16:32:31 -0400

    Reformatting

--- src/main/java/com/mask/demo/service/MaskServiceImpl.java
+++ src/main/java/com/mask/demo/service/MaskServiceImpl.java
@@ -15,24 +15,24 @@ public class MaskServiceImpl implements MaskService {
 		String[] parts = email.split("@");
         
 		String strId = "";
-        if(parts[0].length() < 3) {
+        	if(parts[0].length() < 3) {
 			strId = maskString(parts[0], 0, parts[0].length());
 		} else {
 			strId = maskString(parts[0], 1, parts[0].length() - 1);
 		}
 
-        return strId + "@" + parts[1];
+        	return strId + "@" + parts[1];
 	}
 	
-	private static String maskString(String strText, int start, int end) {
-			int maskLength = end - start;
-	        StringBuilder sbMaskString = new StringBuilder(maskLength);
-	        
-	        for(int i = 0; i < maskLength; i++){
-	            sbMaskString.append('*');
-	        }
-	        
-	        return strText.substring(0, start)   + sbMaskString.toString()  + strText.substring(start + maskLength);
-	    }
+	private String maskString(String strText, int start, int end) {
+		int maskLength = end - start;
+		StringBuilder sbMaskString = new StringBuilder(maskLength);
+		
+		for(int i = 0; i < maskLength; i++){
+			sbMaskString.append('*');
+		}
+		
+		return strText.substring(0, start)   + sbMaskString.toString()  + strText.substring(start + maskLength);
+	}
 
 }
